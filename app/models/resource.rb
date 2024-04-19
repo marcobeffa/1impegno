37f1983b81e93e0a824adf4e22cffc9631345a13
marcobeffa <marcobeffa@gmail.com>
2024-04-19 08:12:07 +0200

    fix

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,8 @@
 class Resource < ApplicationRecord
   belongs_to :user
+  enum tipo: { attività: 0, compiti: 1, contatti: 2, luogo: 2, abilità: 3, ruoli: 4  }
+  enum visibility: { privato: 0, gruppo: 1, pubblico: 2   }
+ 
+
+  validates :data, presence: true
 end
