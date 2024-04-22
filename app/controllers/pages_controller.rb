4f27483b9264912cbaf328a0f4396a2aa449aca6
marcobeffa <marcobeffa@gmail.com>
2024-04-22 13:33:10 +0200

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
-
+  # before_action :newdashval, only: %i[ home dash ]
   def dash
-
     @dashes = Dash.all
   end
 
   def home
+   
+   
     if user_signed_in?
       @contents = current_user.contents
     end
@@ -44,14 +45,51 @@ class PagesController < ApplicationController
     redirect_to dash_path
   end
 
+  private
+
+  def newdashval
+    @dash = Dash.last
+    # crea valore se vuoto
+    if @dash.nil?
+      @dash = Dash.create
+    end
+    
+    # crea valore se creato dopo 24 ore
+    if @dash.created_at < 24.hours.ago
+      @dash = Dash.create
+    end
+    
+    # Controlla se ci sono valori mancanti negli attributi
+    if !@dash.attributes.values.all?(&:present?)
+      # Array dei nomi degli attributi escludendo "id", "updated_at" e "created_at"
+      nomi_attributi_senza_esclusi = Dash.attribute_names - ["id", "updated_at", "created_at"]
+
+      # Ciclo finché ci sono valori mancanti negli attributi
+      while !@dash.attributes.values.all?(&:present?)
+        # Trova il primo attributo con valore mancante
+        attributo_mancante = nomi_attributi_senza_esclusi.find { |attributo| @dash.attributes[attributo].blank? }
+
+        # Se trovi un attributo mancante, ottieni il suo valore e aggiorna il record
+        if attributo_mancante
+          response = HTTParty.get("https://api.coingecko.com/api/v3/simple/price?ids=dash&vs_currencies=#{attributo_mancante}")
+          if response.code == 200
+            data = JSON.parse(response.body)
+            exchange_rate = data['dash']["#{attributo_mancante}"]
+            @dash.update("#{attributo_mancante}": exchange_rate)
+          else
+            Rails.logger.error "Failed to fetch Dash to #{attributo_mancante} conversion rate: #{response.code} - #{response.body}"
+          end
+        else
+          # Se non ci sono più attributi mancanti, esci dal ciclo
+          break
+        end
+      end
+    end
+
+  end
+
   
-
-
-
  
- 
-  
-  
 
   
 end
--- app/jobs/dashvalue_job.rb
+++ app/jobs/dashvalue_job.rb
@@ -1,31 +1,43 @@
 class DashvalueJob < ApplicationJob
   queue_as :default
-  
 
   def perform
-    #  valute = [:eur, :chf, :gbp, :usd, :jpy, :btc, :eth, :ltc, :eos, :dot, :link]
-    val = :eur
-    def valdash(val)
-      response = HTTParty.get("https://api.coingecko.com/api/v3/simple/price?ids=dash&vs_currencies=#{val}")
+    @dash = Dash.last
+    # crea valore se vuoto
+    # if @dash.nil?
+      @dash = Dash.create
+   # end
     
-      if response.code == 200
-        data = JSON.parse(response.body)
-        exchange_rate = data['dash'][val]
+    # crea valore se creato dopo 24 ore
+   # if @dash.created_at < 24.hours.ago
+    #  @dash = Dash.create
+    # end
     
-        Dash.transaction do
-          @dash = Dash.last_or_initialize
-          if @dash.update("#{val}" => exchange_rate)
-            flash[:success] = "Exchange rate for Dash to #{val} updated successfully."
+    # Controlla se ci sono valori mancanti negli attributi
+    if !@dash.attributes.values.all?(&:present?)
+      # Array dei nomi degli attributi escludendo "id", "updated_at" e "created_at"
+      nomi_attributi_senza_esclusi = Dash.attribute_names - ["id", "updated_at", "created_at"]
+
+      # Ciclo finché ci sono valori mancanti negli attributi
+      while !@dash.attributes.values.all?(&:present?)
+        # Trova il primo attributo con valore mancante
+        attributo_mancante = nomi_attributi_senza_esclusi.find { |attributo| @dash.attributes[attributo].blank? }
+
+        # Se trovi un attributo mancante, ottieni il suo valore e aggiorna il record
+        if attributo_mancante
+          response = HTTParty.get("https://api.coingecko.com/api/v3/simple/price?ids=dash&vs_currencies=#{attributo_mancante}")
+          if response.code == 200
+            data = JSON.parse(response.body)
+            exchange_rate = data['dash']["#{attributo_mancante}"]
+            @dash.update("#{attributo_mancante}": exchange_rate)
           else
-            flash[:error] = "Failed to update exchange rate for Dash to #{val}."
+            Rails.logger.error "Failed to fetch Dash to #{attributo_mancante} conversion rate: #{response.code} - #{response.body}"
           end
+        else
+          # Se non ci sono più attributi mancanti, esci dal ciclo
+          break
         end
-      else
-        Rails.logger.error "Failed to fetch Dash to #{val} conversion rate: #{response.code} - #{response.body}"
-        flash[:error] = "Failed to fetch Dash to #{val} conversion rate from Coingecko API."
       end
-    
-      redirect_to dash_path
     end
   end
 end
