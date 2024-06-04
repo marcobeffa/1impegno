6d52a3c828334f3a10f44c928fd3bd622ddbe56f
marcobeffa <marcobeffa@gmail.com>
2024-06-04 08:25:08 +0200

    fix

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :user
-  enum tipo: { attività: 0, processo: 1, contatto: 2,  abilità: 3, luogo: 4, post: 5, todo: 6, obiettivo: 7, contratto: 8, mappe: 9, imprese: 10, progetto: 11, gruppi: 12, calendario: 13  }
+  enum tipo: { attività: 0, processo: 1, contatto: 2,  abilità: 3, luogo: 4, post: 5, todo: 6, obiettivo: 7, contratto: 8, mappe: 9, imprese: 10, progetto: 11, gruppi: 12, calendario: 13, evento: 14   }
   enum visibility: { privato: 0, partecipanti: 1, pubblico: 2   }
   enum stato: { nuovi: 0, to_fix: 1, to_update: 2, attivi: 3, conclusi: 4 }
 
