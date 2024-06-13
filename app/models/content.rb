fc29a400224b4d9ff5b84fc70326319e8107f12c
marcobeffa <marcobeffa@gmail.com>
2024-06-13 18:27:30 +0200

    fix

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,7 @@ class Content < ApplicationRecord
   has_many :linkcontents_as_content_1, class_name: 'Linkcontent', foreign_key: 'content_1_id'
   has_many :linkcontents_as_content_2, class_name: 'Linkcontent', foreign_key: 'content_2_id'
 
-  enum tipo: {gruppo: 0, contatto: 1,  abilità: 2,  impresa: 3, progetto: 4, attività:  5,  processo: 6,   todo:  7, post: 8, contratto: 9,  calendario: 10, evento: 11 , luogo: 12  }
+  enum tipo: {evento: 0, luogo: 1,  impresa: 2, progetto: 3, attività:  4, task: 5, gruppo: 6, contatto: 7, abilità: 8  }
   enum visibility: { privato: 0, partecipanti: 1, pubblico: 2   }
   enum stato: { nuovo: 0, to_fix: 1, to_update: 2, attivo: 3, concluso: 4 }
 
